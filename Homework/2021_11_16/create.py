32d3fdd1b7d129d8bfd150e3ea621973888bf5d7
Mayra Estrella <93684336+mayra84@users.noreply.github>
2021-11-17 10:16:43 -0600

    testing something

--- Homework/2021_11_16/create.py
+++ Homework/2021_11_16/create.py
@@ -1,15 +1,4 @@
-from main import main
-
-# def add_items():
-#     continue_or_quit = input(f'Would you like to add another item to "{grocery_list_name}? ')
-#     options = ['1', '2']
-#     while continue_or_quit not in options:
-#         print('That is not a valid option. Please try again.')
-
-#         if continue_or_quit == '1':
-#             print()# go back to list item
-#         elif continue_or_quit == '2':
-#             main()
+# from main import main
 
 one_grocery_to_rule_them_all = {}
 
@@ -43,62 +32,11 @@ def create():
             create()
         elif continue_or_quit == '3':
             print(one_grocery_to_rule_them_all)
-            main()
+            # HOW TO GO BACK TO MENU()
+            # main()
         elif continue_or_quit not in options:
             print('That is not a valid option. Please try again.')
         
         
         # print(groceries)
         print(one_grocery_to_rule_them_all)
-
-    # def add_items():
-    #     continue_or_quit = create() # continue_or_quit = input(f'Would you like to add another item to "{grocery_list_name}"? \n1 - Yes\n2 - No\n:')
-    #     options = ['1', '2']
-    #     print(continue_or_quit)
-    #         # continue_or_quit = add_items()
-    #         # main()
-    #         # add_items()
-    #         continue_or_quit = add_items()
-    #     return continue_or_quit
-    
-
-    # def add_items():
-        # list_items2 = input('What item would you like to have in this grocery list? ')
-        # groceries[grocery_list_name] = list_items.append(list_items2)
-
-
-        # if continue_or_quit == '1':
-        #     print('1 successful')# go back to list item
-        # elif continue_or_quit == '2':
-        #     print('2 successful')
-        #     # main()
-        #     add_items()
-        # APPEND HERE
-
-
-
-    # I need to make it possible to insert several items in one list
-    # Make a loop for inputting items unless they choose to quit OR to create a different list
-
-# append()
-    # print(groceries)
-
-
-# def entries(name_entry, number_entry):
-# entry = {
-#     "name": "name",
-#     "number": "number"
-# }
-# phone_book.append(entry)
-
-
-# def Add_entry():
-#     name_entry = input("Enter name: ")
-#     number_entry = input("Enter phone number: ")
-#     print(phone_book)
-#     entries(name_entry,number_entry)
-
-# print("this would add an entry")
-
-
-# {'this is a list within a dictionary': ['1','2','3']}
